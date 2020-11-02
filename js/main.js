8db8fb4c514107fbc056b0fd101dd6e8c3443d3d
Antonia P <direiellie@gmail.com>
2020-11-02 15:11:02 +0100

    Revolut

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//JAVASCRIPT
+
+
+$(document).ready(function() {
+
+    $('.text-with-dropdown').mouseover(
+        function() {
+            removeDropwonActive();
+            dropdownActive(this);
+    });
+
+    $(document).click(
+        function() {
+            removeDropwonActive();
+    });
+
+});
+
+function dropdownActive(thisElement) {
+    $(thisElement).children('.dropdown').addClass('active');
+}
+
+function removeDropwonActive() {
+    $('.dropdown.active').removeClass('active');
+
+}
